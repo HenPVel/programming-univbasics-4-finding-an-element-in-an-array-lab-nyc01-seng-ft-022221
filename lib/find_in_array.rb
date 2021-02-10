353567bb7c1e8352eceb004d35fecae39c67807d
Henry Velasquez <hvelas93@gmail.com>
2021-02-10 15:14:31 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,10 +2,11 @@ def find_element_index(array, value_to_find)
   count=0
   while count < array.length do
     result==array[count]
-    count+=1
 
     if result=true
       return array[count]
+    count+=1
+
   end
 
 end
